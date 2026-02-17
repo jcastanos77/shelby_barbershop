ab354a51f334a303af722bafc3ab4c04863e2848
jecastanosm <jecastanosm@concredito.com.mx>
2026-02-17 12:34:16 -0700

    changes

--- functions/index.js
+++ functions/index.js
@@ -204,7 +204,7 @@ exports.exchangeMpCode = onCall(
           grant_type: "authorization_code",
           client_secret: MP_TOKEN,
           code,
-          redirect_uri: "https://shelbybarber.net/mp-callback",
+          redirect_uri: "https://dashboardshelby.netlify.app/mp-callback",
         }
       );
 
